ba31d342eb23a84ecc0e02d4a9f51c0d07d315f0
Rakib Hasan <rakibnkhadija@gmail.com>
2024-04-27 21:19:19 +0600

    vercel install

--- index.js
+++ index.js
@@ -10,11 +10,6 @@ app.use(cors());
 app.use(express.json());
 
 
-// xvjJrJSi55rDg0Xx
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kdbwfxu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
